61aa9a3a78d0cc76a0ee3537dd1ed6d73ce2d970
Baptiste Langlade <baptouuuu@gmail.com>
2020-07-05 12:07:55 +0200

    make sure the setup is run in the home directory

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [[ "$PWD" != "$HOME" ]]; then
+    echo 'setup.sh must be run in your home directory';
+    exit 1;
+fi
+
 # First manual step before invoking this script is to run `xcode-select --install`
 # (it's not done here as it will start an off process installer, that would require
 # somehow to watch that the installer has done its job)
