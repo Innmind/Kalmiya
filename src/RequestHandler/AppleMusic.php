0a13cfb3804242a064a89f97048bb9dd5d616d55
Baptiste Langlade <baptouuuu@gmail.com>
2024-03-31 15:39:33 +0200

    fix request handler

--- src/RequestHandler/AppleMusic.php
+++ src/RequestHandler/AppleMusic.php
@@ -4,15 +4,11 @@ declare(strict_types = 1);
 namespace Innmind\Kalmiya\RequestHandler;
 
 use Innmind\Kalmiya\AppleMusic\SDKFactory;
-use Innmind\HttpFramework\RequestHandler;
-use Innmind\Templating\{
-    Engine,
-    Name,
-};
+use Innmind\Framework\Http\RequestHandler;
 use Innmind\Filesystem\{
     Adapter,
-    File\File,
-    Name as FileName,
+    File,
+    Name,
     Directory,
 };
 use Innmind\IPC\{
@@ -20,31 +16,34 @@ use Innmind\IPC\{
     Process,
     Message\Generic as Message,
 };
-use Innmind\Http\Message\{
+use Innmind\Http\{
     ServerRequest,
     Response,
-    StatusCode,
+    Response\StatusCode,
 };
-use Innmind\Stream\Readable\Stream;
-use Innmind\Immutable\Map;
+use Innmind\Validation\Is;
+use Innmind\Immutable\{
+    Sequence,
+    Predicate\Instance,
+};
 
 final class AppleMusic implements RequestHandler
 {
     private SDKFactory $makeSDK;
-    private Engine $render;
+    private Adapter $templates;
     private Adapter $config;
     private IPC $ipc;
     private Process\Name $cli;
 
     public function __construct(
         SDKFactory $makeSDK,
-        Engine $render,
+        Adapter $templates,
         Adapter $config,
         IPC $ipc,
         Process\Name $cli,
     ) {
         $this->makeSDK = $makeSDK;
-        $this->render = $render;
+        $this->templates = $templates;
         $this->config = $config;
         $this->ipc = $ipc;
         $this->cli = $cli;
@@ -62,42 +61,60 @@ final class AppleMusic implements RequestHandler
     private function authorize(ServerRequest $request): Response
     {
         $sdk = ($this->makeSDK)();
-        /** @var Map<string, mixed> */
-        $parameters = Map::of('string', 'mixed');
 
-        return new Response\Response(
-            $code = StatusCode::of('OK'),
-            $code->associatedReasonPhrase(),
+        return Response::of(
+            StatusCode::ok,
             $request->protocolVersion(),
             null,
-            ($this->render)(
-                new Name('appleMusic.html.twig'),
-                ($parameters)('token', $sdk->jwt()),
-            ),
+            $this
+                ->templates
+                ->get(Name::of('appleMusic.html'))
+                ->keep(Instance::of(File::class))
+                ->match(
+                    static fn($file) => $file->content(),
+                    static fn() => throw new \LogicException('Template not found'),
+                )
+                ->map(static fn($line) => $line->map(
+                    static fn($str) => $str->replace('{{ token }}', $sdk->jwt()),
+                )),
         );
     }
 
     private function save(ServerRequest $request): Response
     {
-        /** @var Directory */
-        $config = $this->config->get(new FileName('apple-music'));
-        /** @psalm-suppress PossiblyInvalidArgument */
-        $this->config->add($config->add(File::named(
-            'user-token',
-            Stream::ofContent($request->form()->get('token')->value()),
-        )));
+        $config = $this
+            ->config
+            ->get(Name::of('apple-music'))
+            ->keep(Instance::of(Directory::class))
+            ->match(
+                static fn($config) => $config,
+                static fn() => Directory::named('apple-music'),
+            );
+        $request
+            ->form()
+            ->get('token')
+            ->keep(Is::string()->asPredicate())
+            ->map(File\Content::ofString(...))
+            ->map(static fn($token) => File::named('user-token', $token))
+            ->map($config->add(...))
+            ->match(
+                $this->config->add(...),
+                static fn() => null,
+            );
 
-        if ($this->ipc->exist($this->cli)) {
-            // as during development we may only start the http server
-            $this
-                ->ipc
-                ->get($this->cli)
-                ->send(Message::of('text/plain', 'ok'));
-        }
+        $_ = $this
+            ->ipc
+            ->get($this->cli)
+            ->flatMap(static fn($process) => $process->send(Sequence::of(
+                Message::of('text/plain', 'ok'),
+            )))
+            ->match(
+                static fn() => null,
+                static fn() => null,
+            );
 
-        return new Response\Response(
-            $code = StatusCode::of('OK'),
-            $code->associatedReasonPhrase(),
+        return Response::of(
+            StatusCode::ok,
             $request->protocolVersion(),
         );
     }
